32eb714f279ea460cd7a1d53fade98e338e9e768
george088 <gga1007001@gmail.com>
2018-11-26 20:58:30 +0300

    получение суммы за период для каждого товара

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -5,8 +5,10 @@ class SalesController < ApplicationController
     #"created_at >= ? AND created_at <= ?", start_date, end_date
     date1 = Date.new(2017,3,1)
     date2 = Date.new(2017,3,10)
-    render json: Good.joins(:day).where(days: {:date => date1..date2})#date: FROM '' TO '2017-03-02'}) #days {date: '2017-03-01'})
-    # render json: Day.where(date: '2017-03-01')#days {date: '2017-03-01'})
-    
+    # render json: Good.joins(:day).where(days: {:date => date1..date2})#date: FROM '' TO '2017-03-02'}) #days {date: '2017-03-01'})
+    # render json: Day.where(:date => date1..date2).group_by(:goods).sum(:revenue)
+    render json: Day.select("good_id as good_id, sum(revenue) as total_price").group("good_id")
+
+    # left_outer_joins(:day).distinct.select('good_id.*, SUM(revenue.*) AS revenue_count').group('good.id')
   end
 end
