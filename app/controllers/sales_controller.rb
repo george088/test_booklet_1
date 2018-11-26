a9c60dd9307aba146bfdd7e9f14835e86b8f4ea8
george088 <gga1007001@gmail.com>
2018-11-27 01:31:09 +0300

    проверка исключительных ситуаций

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -5,12 +5,24 @@ class SalesController < ApplicationController
     # render json: Good.find(1).day.where(date: '2017-03-01')
     #"created_at >= ? AND created_at <= ?", start_date, end_date
     
-    return render json: "error from" unless date_validate(params[:from]) && date_validate(params[:to])
+    unless date_validate(params[:from]) && date_validate(params[:to])
+      hh = {}
+      hh['status'] = :unprocessable_entity
+      hh['message'] = 'Неверный формат дат'
+      return render json: hh
+    end
 
-    date1 = Date.parse(params[:from])
-    date2 = Date.parse(params[:to]) 
+
+    date_from = Date.parse(params[:from])
+    date_to = Date.parse(params[:to]) 
     
-    return render json: "sis'ki" if date1 > date2 
+    unless !(date_from > date_to )
+      hh = {}
+      hh['status'] = :unprocessable_entity
+      hh['message'] = 'Первая дата должна быть меньше(позже) второй'
+      return render json: hh
+    end
+    # return render json: "sis'ki" unless !(date_from > date_to )
     # работает
     # render json: Good.joins(:day).where(days: {:date => date1..date2})#date: FROM '' TO '2017-03-02'}) #days {date: '2017-03-01'})
     # render json: Day.where(:date => date1..date2).select("good_id as good_id, sum(revenue) as total_revenue").group("good_id")
@@ -19,7 +31,7 @@ class SalesController < ApplicationController
     
     items = Good
              .left_joins(:day)
-             .where(days: {:date => date1..date2})
+             .where(days: {:date => date_from..date_to})
              .select('goods.id AS id, goods.title AS title, SUM(days.revenue) AS revenue')
              .group('goods.id')
     hh = {}
@@ -33,10 +45,11 @@ class SalesController < ApplicationController
 
     render json: hh
   end
+  
 
-  private
-  def date_validate date
-    Date.parse date rescue nil
-  end
+    private
 
+    def date_validate date
+      Date.parse date rescue nil
+    end
 end
