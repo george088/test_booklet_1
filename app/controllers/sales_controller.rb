06ca546fe93faf22fb63de64e4f0d60948e84eeb
george088 <gga1007001@gmail.com>
2018-11-26 20:34:43 +0300

    получил правильную запись запроса

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,6 +1,12 @@
 class SalesController < ApplicationController
   def sales
 
-    render json: Good.find(1).day.where(date: '2017-03-01')
+    # render json: Good.find(1).day.where(date: '2017-03-01')
+    #"created_at >= ? AND created_at <= ?", start_date, end_date
+    date1 = Date.new(2017,3,1)
+    date2 = Date.new(2017,3,10)
+    render json: Good.joins(:day).where(days: {:date => date1..date2})#date: FROM '' TO '2017-03-02'}) #days {date: '2017-03-01'})
+    # render json: Day.where(date: '2017-03-01')#days {date: '2017-03-01'})
+    
   end
 end
