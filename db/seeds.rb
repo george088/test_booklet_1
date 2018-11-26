675652f442460463050303017c5b6ee04a7a4d4b
george088 <gga1007001@gmail.com>
2018-11-26 13:28:52 +0300

    init 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,77 @@
 require 'roo'
 
 spreadsheet = Roo::Spreadsheet.open('app/assets/data/goods.xlsx')
-spreadsheet.each_row do |row|
-  puts row.inspect # Array of Excelx::Cell objects
+# spreadsheet.each_row do |row|
+#   puts row.inspect # Array of Excelx::Cell objects
+# end
+# puts spreadsheet.row(1)
+header =  spreadsheet.first_row
+# puts spreadsheet.row(header)
+puts row = Hash[header, spreadsheet.row(2)]
+puts row.inspect 
+
+puts 'End'
+puts spreadsheet.row(2)
+
+(2..spreadsheet.last_row).each do |i|
+  Spill.create(
+    title: spreadsheet.row(i)[0]
+    2017-03-01: spreadsheet.row(i)[1],
+    2017-03-02: spreadsheet.row(i)[2],
+    2017-03-03: spreadsheet.row(i)[3],
+    2017-03-04: spreadsheet.row(i)[4],
+    2017-03-05: spreadsheet.row(i)[5],
+    2017-03-06: spreadsheet.row(i)[6],
+    2017-03-07: spreadsheet.row(i)[7],
+    2017-03-08: spreadsheet.row(i)[8],
+    2017-03-09: spreadsheet.row(i)[9],
+    2017-03-10: spreadsheet.row(i)[10],
+    2017-03-11: spreadsheet.row(i)[11],
+    2017-03-12: spreadsheet.row(i)[12],
+    2017-03-13: spreadsheet.row(i)[13],
+    2017-03-14: spreadsheet.row(i)[14],
+    2017-03-15: spreadsheet.row(i)[15],
+    2017-03-16: spreadsheet.row(i)[16],
+    2017-03-17: spreadsheet.row(i)[17],
+    2017-03-18: spreadsheet.row(i)[18],
+    2017-03-19: spreadsheet.row(i)[19],
+    2017-03-20: spreadsheet.row(i)[20],
+    2017-03-21: spreadsheet.row(i)[21] 
+    )
 end
-puts spreadsheet.row(1)
+
+# (2..spreadsheet.last_row).each do |i|
+
+
+#   row = Hash[header, spreadsheet.row(i)]
+#   # Good.create
+#   # Good = find_by_id(row["id"]) || new
+#   Good.attribute = row.to_hash.slice(*accessible_attributes)
+#   Good.save!
+#   # puts row
+# end
+
+# (2..spreadsheet.last_row).each do |i|
+#   row = Hash[header, spreadsheet.row(i)]
+#   Good.create
+#   # product = find_by_id(row["id"]) || new
+#   Good.attributes = row.to_hash.slice(*accessible_attributes)
+#   Good.save!
+#   # puts row
+# end
+
+# CSV.foreach('SeedsSpill.csv') do |row|
+#   url = row[0]
+#   reported = row[1]
+#   incident = row[2]
+#   county = row[3]
+#   lat = row[4].to_f
+#   long = row[5].to_f
+#   contaminant = row[6]
+#   volume = row[7].to_i
+#   units = row[8]
+#   contained = row[9]
+#   user_id = row[10].to_i
+#   Spill.create(IncidentURL: url, DateReported: reported, DateIncident: incident, County: county, Latitude: lat, Longitude: long, Contaminant: contaminant, Volume: volume, Units: units, Contained: contained, user_id: user_id,) 
+# end
+# puts "done"
