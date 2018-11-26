a8bb6875b7757d2f2394cf0f34b2a1bf874967a1
george088 <gga1007001@gmail.com>
2018-11-26 23:49:23 +0300

    1

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -6,9 +6,8 @@ class SalesController < ApplicationController
     date1 = Date.new(2017,3,1)
     date2 = Date.new(2017,3,10)
     # render json: Good.joins(:day).where(days: {:date => date1..date2})#date: FROM '' TO '2017-03-02'}) #days {date: '2017-03-01'})
-    # render json: Day.where(:date => date1..date2).group_by(:goods).sum(:revenue)
-    render json: Day.where(:date => date1..date2).select("good_id as good_id, sum(revenue) as total_revenue").group("good_id")
-
+    # render json: Day.where(:date => date1..date2).select("good_id as good_id, sum(revenue) as total_revenue").group("good_id")
+    render json: Good.includes(:day).where(days: {:date => date1..date2})
     # left_outer_joins(:day).distinct.select('good_id.*, SUM(revenue.*) AS revenue_count').group('good.id')
   end
 end
