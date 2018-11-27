e5e0e52d2338f933486ca6dd2ebcc2be3775cf4c
george088 <gga1007001@gmail.com>
2018-11-28 01:12:41 +0300

    1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,9 +23,8 @@ function refresh()
     data: {from: $('#date_from').val(), to: $('#date_to').val()},
     cache: false,
     success: function(response){
-      console.log(response);
       $('#table_title').empty()
-      
+
       for(var i = 0; i < response.goods.length; i++) {
         $('#table_title').append('<tr><td>' + response.goods[i][1] + '</td><td>' + response.goods[i][2] + '</td></tr>');
       }
