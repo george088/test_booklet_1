665b23592693fc3532ef5074a3f4b727aac6b55a
george088 <gga1007001@gmail.com>
2018-11-28 03:50:08 +0300

    исправление роутов

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'api_sales' => 'sales#api_sales'
-  get 'html_sales' => 'sales#html_sales'
+  get 'api_goods' => 'goods#api_sales'
+  get 'html_sales' => 'goods#html_sales'
 
-  resources :good do 
+  resources :goods do 
     resource :days, only: [:create]
   end
 end
