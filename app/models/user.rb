5fe20fd9937c4f4444dc08929c32f995f1c81c12
george088 <gga1007001@gmail.com>
2018-11-30 15:48:48 +0300

    настройка user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+
+  before_validation { 
+    (self.email = self.email.to_s.downcase) && (self.username = self.username.to_s.downcase) 
+  }
+
+  def is_admin?
+    role == 'admin'
+  end
+
 end
