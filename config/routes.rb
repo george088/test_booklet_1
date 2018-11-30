e7a334f571962b845abab09d0c4983ee3b20977f
george088 <gga1007001@gmail.com>
2018-11-30 22:34:16 +0300

    upd

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # post 'user_token' => 'user_token#create'
+  post 'user_token' => 'user_token#create'
   # get 'user_token' => 'user_token#index'
-  resources :user_token
+  # resources :user_token
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'sales' => 'goods#api_sales'
   get 'html_sales' => 'goods#html_sales'
