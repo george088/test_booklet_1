2d6cc5f45903a3aa2ddbbbcec8ec36b51a662296
george088 <gga1007001@gmail.com>
2018-11-30 22:33:20 +0300

    clear user_token

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,25 +1,25 @@
 class UserTokenController < Knock::AuthTokenController
-  skip_before_action :verify_authenticity_token
-  # before_action :users, only: [:show, :update, :destroy]
+#   skip_before_action :verify_authenticity_token
+#   # before_action :users, only: [:show, :update, :destroy]
 
-  def index
-    render json: "sdfsdfsd"
-  end
+#   def index
+#     render json: "sdfsdfsd"
+#   end
 
-  def create
-    puts "1"
-    user = User.new(user_token_params_permited)
-    puts "2"
-    if user.save
-      puts "3"
-      render json: {status: 200, msg: 'User was created.'}
-    end
-    puts "4"
-  end
+#   def create
+#     puts "1"
+#     user = User.new(user_token_params_permited)
+#     puts "2"
+#     if user.save
+#       puts "3"
+#       render json: {status: 200, msg: 'User was created.'}
+#     end
+#     puts "4"
+#   end
 
-private
+# private
 
-  def user_token_params_permited
-    params.require(:user).permit(:email, :password)
-  end
+#   def user_token_params_permited
+#     params.require(:user).permit(:email, :password)
+#   end
 end
