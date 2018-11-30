079ba9bb64577a4aa93c557150d13fba05ee336b
george088 <gga1007001@gmail.com>
2018-11-30 16:19:49 +0300

    username downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   before_validation { 
-    (self.email = self.email.to_s.downcase) && (self.username = self.username.to_s.downcase) 
+    self.username = self.username.to_s.downcase 
   }
 
   def is_admin?
