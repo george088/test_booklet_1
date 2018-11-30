6faf07012ae8d05690f27899e76fe1f823e47a2a
george088 <gga1007001@gmail.com>
2018-11-30 16:17:32 +0300

    упростил. удаление переменных

--- db/schema.rb
+++ db/schema.rb
@@ -29,13 +29,10 @@ ActiveRecord::Schema.define(version: 2018_11_30_123120) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.string "email", null: false
     t.string "password_digest"
     t.string "role", default: "admin", null: false
-    t.datetime "last_login"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
   end
 
 end
