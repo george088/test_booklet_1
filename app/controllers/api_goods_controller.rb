4e881fa20189196b07e788f06617956fd66063ed
george088 <gga1007001@gmail.com>
2018-11-30 22:29:53 +0300

    1

--- app/controllers/api_goods_controller.rb
+++ app/controllers/api_goods_controller.rb
@@ -1,5 +1,18 @@
 class ApiGoodsController < ApplicationController
 
+  # before_action :authenticate_user,  only: [:auth]
+
+  # # Public method
+  # def index
+  #   render json: { service: 'auth-api', status: 200 }
+  # end
+  
+  # # Authorized only method
+  # def auth
+  #   render json: { status: 200, msg: "You are currently Logged-in as #{current_user.username}" }
+  # end
+
+
   def index
     @items = Good.all
     json_response(@items)
