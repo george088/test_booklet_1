d141291bec9fc95338057174b521d69dd0e2824d
george088 <gga1007001@gmail.com>
2018-11-30 17:17:15 +0300

    разрешение емейла

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,6 +1,6 @@
 class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token
-  
+
   def create
     user = User.new(user_token_params_permited)
     if user.save
@@ -11,6 +11,6 @@ class UserTokenController < Knock::AuthTokenController
 private
 
   def user_token_params_permited
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :email)
   end
 end
