e471fed07ebdccb2636566a386a67eeaa9cd1999
george088 <gga1007001@gmail.com>
2018-12-01 23:15:38 +0300

    проперка валидаций и уникального email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,18 @@
 require "rails_helper"
 
-describe User do
-  describe "validations" do
-    it { should validate_presence_of(:email) }
-    # it { should validate_presence_of(:password) }
-  end 
+describe User, type: :model do
 
+  describe 'Setting up model' do
+    describe "validation email" do
+      it { should validate_presence_of(:email) }
+    end 
+
+    describe "uniq email" do
+      it { should validate_uniqueness_of(:email) }
+    end
+
+    describe "validation password" do
+      it { should validate_presence_of(:password) }
+    end
+  end
 end
