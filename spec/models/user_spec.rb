c20ce791a1ec232a25c8ed63a87d8887cd2d86e5
george088 <gga1007001@gmail.com>
2018-12-01 23:31:28 +0300

    убрал лишнюю вложенность. добавил проверку роли по умолчанию

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,16 @@ require "rails_helper"
 describe User, type: :model do
 
   describe 'Setting up model' do
-    describe "validation email" do
-      it { should validate_presence_of(:email) }
-    end 
+    it { should validate_presence_of(:email) }
 
-    describe "uniq email" do
-      it { should validate_uniqueness_of(:email) }
-    end
+    it { should validate_uniqueness_of(:email) }
 
-    describe "validation password" do
-      it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:password) }
+
+    it "check default role" do
+    # User.new(email: 'admin@admin', password: 'sdf')
+      expect(User.new.role).to eq "admin"
     end
   end
+
 end
