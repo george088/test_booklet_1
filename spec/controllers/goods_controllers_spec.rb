22c937b20ba8aae0b99da9227a9adbacb8318533
george088 <gga1007001@gmail.com>
2018-12-03 00:05:30 +0300

    добавление before

--- spec/controllers/goods_controllers_spec.rb
+++ spec/controllers/goods_controllers_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 describe GoodsController do 
+  before(:each) do
+    @item = create(:good)
+  end
+  
   describe "#html_sales" do
     it "render html_render" do
       get :html_sales
@@ -22,65 +26,68 @@ describe GoodsController do
     end
 
     it "redirect_to goods_path if nothing" do
-      item = create(:good)
-      get :show, params: { id: item.id }
+      get :show, params: { id: @item.id }
       response.should render_template
     end
   end
 
   describe "#edit" do
-    item_edit = create(:good)
-
     it "redirect_to goods_path if nothing" do
       get :edit, params: { id: '0' }
-        response.should redirect_to(goods_path)
+      response.should redirect_to(goods_path)
     end
 
     it "redirect_to goods_path if nothing" do
-      get :edit, params: { id: item_edit.id }
+      get :edit, params: { id: @item.id }
       response.should render_template
     end
   end
 
   describe "#update" do # нужно как-то поменять логику
-    item_update = create(:good)
-
     it "redirect_to item_update" do
-      get :update, params: { id: item_update.id }
-        response.should redirect_to(item_update)
+      get :update, params: { id: @item.id }
+      response.should redirect_to(@item)
     end
 
     it "render action edit if nothing" do
       get :update, params: { id: '0' }
-        response.should render_template('edit')
+      response.should render_template('edit')
     end
   end
 
   describe "#сreate" do
-    item_сreate = create(:good)
-
     it "redirect_to item_сreate" do
-      get :create, params: { id: item_сreate.id }
-        response.should redirect_to(item_сreate)
+      get :create, params: { id: @item.id }
+      response.should redirect_to(@item)
     end
 
     it "render action new if nothing" do
       get :create
-        response.should render_template('new')
+      response.should render_template('new')
     end
   end
 
   describe "#destroy" do
-    item_destroy = create(:good)
-
     it "redirect to goods_path if ok" do
       get :destroy
-        response.should redirect_to(goods_path) if item_destroy.destroy
+      response.should redirect_to(goods_path) if item_destroy.destroy
     end
 
     it "redirect to item if not" do
       get :destroy
-        response.should redirect_to(goods_path) if !item_destroy.destroy
+      response.should redirect_to(goods_path) if !item_destroy.destroy
+    end
+  end
+
+  describe "#api_sales" do
+    it "redirect to goods_path if ok" do
+      get :destroy
+      response.should redirect_to(goods_path) if @item.destroy
+    end
+
+    it "redirect to item if not" do
+      get :destroy
+      response.should redirect_to(goods_path) if !@item.destroy
     end
   end
 end
